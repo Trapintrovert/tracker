aa904ad94a34ad6fa053a2d86f413c02f5df6bc1
trapintrovert <mcneo062@gmail.com>
2021-02-05 16:52:46 +0100

    Axios install and signup context setup

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL: 'http://2ae3a169f4a0.ngrok.io'
+})
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext'
+import trackerApi from '../api/tracker'
 
 const authReducer = (state, action) => {
     switch (action.type) {
@@ -7,9 +8,34 @@ const authReducer = (state, action) => {
     }
 }
 
+const signup = dispatch => {
+    return async ({ email, password }) => {
+        try {
+            const response = await trackerApi.post('/signup', {email, password})
+            console.log(response.data)
+        } catch (err) {
+            console.log(err.response.data)
+        }
+    }
+}
+
+
+const signin = (dispatch) => {
+    return ({email, password}) => {
+        // Try to signin
+        // Handle success by updating state
+        // Handle failure by showing error message (somehow)
+    }
+}
+
+const signout = (dispatch) => {
+    return () => {
+        // somehow sign out!!
+    }
+}
 
 export const { Provider, Context} = createDataContext(
     authReducer,
-    {},
+    {signin, signout, signup},
     { isSignedIn: false }
 )
