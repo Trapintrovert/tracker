014198836e33282922d76ff9ca7c0a7f77dec48b
trapintrovert <mcneo062@gmail.com>
2021-02-10 13:39:58 +0100

    SaveView added to AccountScreen

--- src/screen/AccountScreen.js
+++ src/screen/AccountScreen.js
@@ -7,7 +7,7 @@ import { Context as AuthContext } from '../context/AuthContext'
 const AccountScreen = () => {
     const { signout } = useContext(AuthContext)
     return (
-        <SafeAreaView>
+        <SafeAreaView forceInset={{ top: 'always' }}>
             <Text style={{ fontSize: 48}}>Account Screen</Text>
             <Spacer>
             <Button title="Sign out" onPress={signout}/>
