d5a0378c58986c82d304cdb739bb055b705fef99
trapintrovert <mcneo062@gmail.com>
2021-02-11 14:11:30 +0100

    Circle indicator added to Map

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
-import MapView, { Polyline } from 'react-native-maps'
+import MapView, { Polyline, Circle } from 'react-native-maps'
 import { Context as LocationContext } from '../context/LocationContext'
 
 
@@ -19,9 +19,13 @@ const Map = () => {
                 latitudeDelta: 0.01,
                 longitudeDelta: 0.01
             }}
-           
         >
-            
+            <Circle
+                center={currentLocation.coords}
+                radius={35}
+                strokeColor="rgba(158, 158, 255, 1.0)"
+                fillColor="rgba(158, 158, 255, 0.3)"
+            />
         </MapView>
     )
 }
