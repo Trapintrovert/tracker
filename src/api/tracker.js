a331ac5f82b85a86180fe171fe836aa884579a4e
trapintrovert <mcneo062@gmail.com>
2021-02-12 14:58:52 +0100

    Automatic Authentication added

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
-    baseURL: 'http://2ad70d984ba4.ngrok.io'
+const instance = axios.create({
+    baseURL: 'http://30969f917787.ngrok.io'
 })
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token')
+        if(token){
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (err) => {
+        return Promise.reject(err)
+    }
+)
+
+export default instance
